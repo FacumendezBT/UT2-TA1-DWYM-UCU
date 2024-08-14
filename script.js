eb17f67eb17f141fc698db6d5a7fb35c4e53657d
Facundo Méndez Bentos <facumendezbt@gmail.com>
2024-08-14 10:35:43 -0300

    Hagamos las cosas bien...

--- script.js
+++ script.js
@@ -1,12 +1,19 @@
 
-let btn = document.getElementById('btn');
-let texto = document.getElementById('texto');
-let repeticiones = document.getElementById('repeticiones');
+const btn = document.getElementById('btn');
+const texto = document.getElementById('texto');
+const repeticiones = document.getElementById('repeticiones');
 
 btn.addEventListener('click', function(){
-    repeatString(texto.value, repeticiones.value);
+    let textoValue = texto.value;
+    let repeticionesValue = repeticiones.value;
+    repeatString(textoValue, repeticionesValue);
 });
 
+/**
+ * Función que imprime un texto en consola n veces
+ * @param {string} texto - Texto a imprimir
+ * @param {number} repeticiones - Número de veces que se imprimirá el texto
+ */
 function repeatString(texto, repeticiones){
     for (let i = 0; i < repeticiones; i++){
         console.log(texto);
